9f3a62c2e204ed84f55d3dc7a5823993a9853de7
edward <reddtoric@users.noreply.github.com>
2021-06-24 13:21:52 -0500

    add form validation and add list of country for country input

--- CityFinder/ClientApp/src/components/FetchData.js
+++ CityFinder/ClientApp/src/components/FetchData.js
@@ -2,57 +2,103 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Autocomplete from "@material-ui/lab/Autocomplete";
 import Map from './Map';
 
 const styles = theme => ({
-    textFieldShort: {
-        margin: theme.spacing(1),
-        width: '15ch',
-    },
-    textFieldLong: {
+    textField: {
         margin: theme.spacing(1),
         width: '25ch',
     },
     button: {
         margin: theme.spacing(1),
+    },
+    option: {
+        fontSize: 15,
+        "& > span": {
+            marginRight: 10,
+            fontSize: 18
+        }
     }
 });
 
+const defaultCountry = {
+    "alpha2Code": "US",
+    "name": "United States of America"
+};
 
-export default withStyles(styles)(
+// ISO 3166-1 alpha-2
+// !!! No support for IE 11
+function countryToFlag(isoCode) {
+    return typeof String.fromCodePoint !== "undefined"
+        ? isoCode
+            .toUpperCase()
+            .replace(/./g, (char) =>
+                String.fromCodePoint(char.charCodeAt(0) + 127397)
+            )
+        : isoCode;
+}
+
     class FetchData extends Component {
         static displayName = FetchData.name;
 
         constructor(props) {
             super(props);
             this.state = {
-                location: null,
-                loading: true,
+                isCityFound: false,
+                locationObj: null,
+                countries: [],
                 zipcode: '',
-                country: 'US'
+                countryObj: defaultCountry,
+                isCountryEmpty: false,
             };
 
             this.handleChangeZip = this.handleChangeZip.bind(this);
             this.handleChangeCountry = this.handleChangeCountry.bind(this);
             this.handleSubmit = this.handleSubmit.bind(this);
             this.handleClearInput = this.handleClearInput.bind(this);
+
+            this.getCountries();
+        }
+
+        componentDidMount() {
+            this.zipcodeInput.focus();
         }
 
         handleChangeZip(event) {
             this.setState({ zipcode: event.target.value });
         }
-
-        handleChangeCountry(event) {
-            this.setState({ country: event.target.value });
+        
+        handleChangeCountry(event, newValue) {
+            this.setState({ countryObj: newValue, isCountryEmpty: newValue === null });
         }
 
         handleSubmit(event) {
             event.preventDefault();
-            this.populateData();
-        }   
+            this.findCity();
+        }
 
         handleClearInput(event) {
-            this.setState({ zipcode: "", country: "" });
+            this.setState({ zipcode: "", countryObj: null });
+        }
+
+        async findCity() {
+            const response = await fetch('api/city?country=' + this.state.countryObj.alpha2Code + '&zipcode=' + this.state.zipcode);
+            if (response.status === 200) {
+                const data = await response.json();
+                this.setState({ location: data, isCityFound: true });
+            }
+            else {
+                this.setState({ location: null, isCityFound: false });
+            }
+        }
+
+        async getCountries() {
+            const response = await fetch('api/countries');
+            if (response.ok) {
+                const data = await response.json();
+                this.setState({ countries: data, countriesLoading: false });
+            }
         }
 
         static renderMap(location) {
@@ -85,11 +131,11 @@ export default withStyles(styles)(
         render() {
             const { classes } = this.props;
 
-            let contents = this.state.loading
-                ? <p><em>No search results found.</em></p> 
+            let contents = !this.state.isCityFound
+                ? <p><em>No search results found.</em></p>
                 : FetchData.renderTable(this.state.location);
 
-            let map = this.state.loading
+            let map = !this.state.isCityFound
                 ? <p><em>No search results for map.</em></p>
                 : FetchData.renderMap(this.state.location);
 
@@ -98,23 +144,52 @@ export default withStyles(styles)(
                     <h1 id="tableLabel" >City Finder</h1>
                     <form onSubmit={this.handleSubmit} autoComplete="off">
                         <div>
-                            <TextField
-                                className={classes.textFieldShort} 
-                                required
-                                id="outlined-required"
-                                label="2-letter Country Code"
-                                value={this.state.country}
+                            <Autocomplete
+                                autoHighlight
+                                style={{ width: 400 }}
+                                options={this.state.countries}
+                                classes={{
+                                    option: classes.option
+                                }}
+                                renderOption={(option) => (
+                                    <React.Fragment>
+                                        <span>{countryToFlag(option.alpha2Code)}</span>
+                                        {option.name}
+                                    </React.Fragment>
+                                )}
+                                renderInput={(params) => (
+                                    <TextField
+                                        {...params}
+                                        required
+                                        error={this.state.countryObj === null}
+                                        label="Choose a country"
+                                        variant="outlined"
+                                        inputProps={{
+                                            ...params.inputProps,
+                                            autoComplete: "new-password"
+                                        }}
+                                    />
+                                )}
+                                getOptionLabel={(option) => `${option.alpha2Code}  ${option.name}`}
+                                getOptionSelected={(option) => option.alpha2Code}
+                                defaultValue={defaultCountry}
+                                value={this.state.countryObj}
                                 onChange={this.handleChangeCountry}
-                                variant="outlined"
                             />
+
                             <TextField
-                                className={classes.textFieldLong} 
+                                inputRef={(input) => { this.zipcodeInput = input; }}
+                                className={classes.textField}
                                 required
+                                error={this.state.zipcode === ''}
                                 id="outlined-required"
                                 label="Zip Code"
                                 value={this.state.zipcode}
                                 onChange={this.handleChangeZip}
                                 variant="outlined"
+                                inputProps={{
+                                    maxLength: 10
+                                }}
                             />
                         </div>
                         <div>
@@ -140,16 +215,6 @@ export default withStyles(styles)(
                 </div>
             );
         }
+}
 
-        async populateData() {
-            const response = await fetch('api/cityfinder?country=' + this.state.country + '&zipcode=' + this.state.zipcode);
-            if (response.status === 200) {
-                const data = await response.json();
-                this.setState({ location: data, loading: false });
-            }
-            else {
-                this.setState({ location: null, loading: true });
-            }
-        }
-    }
-)
+export default withStyles(styles)(FetchData);
