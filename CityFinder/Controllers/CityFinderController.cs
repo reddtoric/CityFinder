0285202795badb660204dda16da1101c4bd0e601
edward <reddtoric@users.noreply.github.com>
2021-06-24 19:53:28 -0500

    add validation for api query

--- CityFinder/Controllers/CityFinderController.cs
+++ CityFinder/Controllers/CityFinderController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CityFinder.Business;
 using CityFinder.Models;
@@ -21,6 +22,18 @@ namespace CityFinder.Controllers
         [HttpGet("city", Name ="GetCity")]
         public async Task<ActionResult<Location>> Get(string country, string zipcode)
         {
+            // Valid zipcode is alphanumeric and max 10 characters long
+            Regex regexValidZipcode = new Regex("^[a-zA-Z0-9]{0,10}$");
+
+            // Valid country code is 2 letters
+            Regex regexValidCountry = new Regex("^[a-zA-Z]{2}$");
+
+            // Invalid country and/or zipcode query
+            if (!regexValidZipcode.IsMatch(zipcode) || !regexValidCountry.IsMatch(country))
+            {
+                return BadRequest();
+            }
+
             return Ok(await _cityFinderLogic.GetCity(new Query() { CountryCode = country, ZipCode = zipcode }));
         }
 
