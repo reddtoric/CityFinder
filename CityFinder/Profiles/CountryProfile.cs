e66bd5927182bb48ccaceb6d9d0f74b16b171fa4
edward <reddtoric@users.noreply.github.com>
2021-09-30 15:03:46 -0500

    use updated api, automap country to dto

---
+++ CityFinder/Profiles/CountryProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CityFinder.Models;
+using CityFinder.Dtos;
+
+namespace CityFinder.Profiles
+{
+    public class CountryProfile : Profile
+    {
+        public CountryProfile()
+        {
+            CreateMap<Country, CountryDto>()
+                .ForMember(dest => dest.Alpha2Code, opt => opt.MapFrom(src => src.Cca2))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.Offical));
+        }
+    }
+}
